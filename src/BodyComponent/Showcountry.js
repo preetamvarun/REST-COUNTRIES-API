e46137419770b070f00f06d5756b649360655ae4
Preetam Varun <preetamarun99@gmail.com>
2023-02-07 16:05:26 +0000

    routing is working

--- src/BodyComponent/Showcountry.js
+++ src/BodyComponent/Showcountry.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useShowCountry } from "../Hooks/useShowCountry";
+import { Link } from "react-router-dom";
 
 
 const Showcountry = () => {
@@ -7,27 +8,30 @@ const Showcountry = () => {
 
     const data = useShowCountry(name);
 
-    console.log(data);
-
     return data.length === 0 ? <h1>Getting the data</h1> : (
-        <div className="bg-slate-800 text-white">
+        <div>
+            <Link to = '/'>Back</Link>
+            <div className="bg-slate-800 text-white">
             <img src={data[0].flags.svg} alt = {name}/>
             <div>
-                <p>name</p> 
-                <p>Native name :</p>
-                <p>Population : </p>
-                <p>Region : </p>
-                <p>Sub Region : </p>
-                <p>Capital : </p>
+                <p>name : {data[0].name.official}</p> 
+                <p>Native name : {
+                    Object.values(data[0].name.nativeName).splice(-1)[0].official
+                }</p>
+                <p>Population : {data[0].population}</p>
+                <p>Region : {data[0].region} </p>
+                <p>Sub Region : {data[0].subregion} </p>
+                <p>Capital : {data[0].capital[0]} </p>
             </div>
             <div>
-                <p>Top Level Domain : </p>
-                <p>Currencies : </p>
-                <p>Languages : </p>
+                <p>Top Level Domain : {data[0].tld[0]}</p>
+                <p>Currency : {Object.values(data[0].currencies)[0].name}</p>
+                <p>Languages : {Object.values(data[0].languages).join(',')}</p>
             </div>
             {
                 data[0].hasOwnProperty('borders') && data[0].borders.map((eachBorder) => <p>{eachBorder}</p>)
             }
+        </div>
         </div>
     )
 }
