636d9370353dfd05867ff534f876d3294baddf8a
Preetam Varun <preetamarun99@gmail.com>
2023-02-10 18:36:53 +0000

    will continue working tommorow

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Header from "./HeaderComponent/Header";
-import Countries from "./BodyComponent/Countries";
-import Showcountry from "./BodyComponent/Showcountry";
-import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
-import { Error } from "./Error";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './HeaderComponent/Header';
+import Countries from './BodyComponent/Countries';
+import Showcountry from './BodyComponent/Showcountry';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Error } from './Error';
 
 const AppLayout = () => {
-    return(
-        <React.Fragment>
-            <Header/>
-            <Outlet/>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <Header />
+      <Outlet />
+    </React.Fragment>
+  );
+};
 
 const appRouter = createBrowserRouter([
-    {
-        path : '/',
-        element : <AppLayout/>,
-        errorElement : <Error/>,
-        children : [
-            {
-                path : '/',
-                element : <Countries/>
-            },
-            {
-                path : '/country/:name',
-                element : <Showcountry/>
-            }
-        ]
-    }
-])
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Countries />
+      },
+      {
+        path: '/country/:name',
+        element: <Showcountry />
+      }
+    ]
+  }
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(<AppLayout/>)
 
-root.render(<RouterProvider router = {appRouter}/>)
+root.render(<RouterProvider router={appRouter} />);
--- src/Hooks/useFilterCountries.js
+++ src/Hooks/useFilterCountries.js
@@ -2,9 +2,12 @@ function useFilterCountries(searchCountry, allCountries, region) {
   const data = allCountries.filter(eachCountry =>
     eachCountry.name.common.toLowerCase().includes(searchCountry.toLowerCase())
   );
+
   if (data.length == 0 || region === 'Filter By Region') return data;
-  const data1 = data.filter(eachCountry => eachCountry.region === region);
-  return data1;
+
+  // If the region is not 'Filter By Region'
+
+  return data.filter(eachCountry => eachCountry.region === region);
 }
 
 export default useFilterCountries;
