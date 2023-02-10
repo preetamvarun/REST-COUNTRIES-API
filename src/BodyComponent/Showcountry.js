eb5181ac7f4bf22b6985e91f09bfbb6c245e4089
Preetam Varun <preetamarun99@gmail.com>
2023-02-10 10:47:08 +0000

    styled show country page. Still need to add borders and style them

--- src/BodyComponent/Showcountry.js
+++ src/BodyComponent/Showcountry.js
@@ -8,39 +8,45 @@ const Showcountry = () => {
 
     const data = useShowCountry(name);
 
-    console.log(data);
-
-
-    return Object.keys(data).length === 0 ? <h1>Getting the data</h1> : (
-        <div className="bg-slate-800 text-white">
-            <Link to = '/' className="inline-block">Back</Link>
-            <div className=" text-white flex justify-evenly items-center">
-                <div className="bg-orange-600">
-                    <img src={data[0].flags.svg} alt = {name}/>
-                </div>
-                <div className="bg-red-500">
-                    <p className="font-bold py-[2px]">{data[0].name.official}</p> 
-                    <p className="py-[2px]">Native name : {
-                        Object.values(data[0].name.nativeName).splice(-1)[0].official
-                    }</p>
-                    <p className="py-[2px]">Population : {data[0].population}</p>
-                    <p className="py-[2px]">Region : {data[0].region} </p>
-                    <p className="py-[2px]">Sub Region : {data[0].subregion} </p>
-                    <p className="py-[2px]">Capital : {data[0].capital[0]} </p>
-                </div>
-                <div className="bg-green-500">
-                    <p>Top Level Domain : {data[0].tld[0]}</p>
-                    <p>Currency : {Object.values(data[0].currencies)[0].name}</p>
-                    <p>Languages : {Object.values(data[0].languages).join(',')}</p>
-                </div>
+    return data.length === 0 ? <h1>Getting the data</h1> : (
+        <div className="text-white">
+            <div className="inline-block bg-slate-600 py-2 px-8 m-8 rounded-md hover:cursor-pointer">
+                <i className="fa-solid fa-arrow-left mr-1"></i>
+                <Link to='/' className="text-white inline-block">Back</Link>
             </div>
-        <div className="text-white bg-orange-800 w-[30%]">
-                {
-                    data[0].hasOwnProperty('borders') && data[0].borders.map((eachBorder) => <p key={uuidv4()} className = 'inline-block p-1'>{eachBorder}</p>)
-                }
-        </div>
+            <div className="flex justify-evenly items-center relative">
+                <div>
+                    <img src={data[0].flags.svg} alt = {name} className = 'h-[250px] w-[320px]'/>
+                </div>
+                <div className="mb-3">
+                    <p className="font-bold py-[2px]">{data[0].name.official}</p> 
+                    <div className="mt-3">
+                        <p className="py-[2px]"> <span className="font-medium">Native name :</span> {
+                            Object.values(data[0].name.nativeName).splice(-1)[0].official
+                        }</p>
+                        <p className="py-1"><span className="font-medium">Population : </span>{data[0].population}</p>
+                        <p className="py-1"><span className="font-medium">Region : </span>{data[0].region} </p>
+                        <p className="py-1"><span className="font-medium">Sub Region : </span>{data[0].subregion} </p>
+                        <p className="py-1"><span className="font-medium">Capital : </span>{data[0].capital[0]} </p>
+                    </div>
+                </div>
+                <div>
+                    <p><span className="font-semibold">Top Level Domain : </span>{data[0].tld[0]}</p>
+                    <p className="py-1"><span className="font-semibold">Currency : </span> {Object.values(data[0].currencies)[0].name}</p>
+                    <p><span className="font-semibold">Languages : </span>{Object.values(data[0].languages).join(',')}</p>
+                </div>
+                {/*
+                    <div className="text-white left-1/2 bottom-0 bg-orange-500 absolute w-[30%]">
+                        <span className="font-medium pl-1 text-sm">Border Countries : </span>
+                        {
+                            data[0].hasOwnProperty('borders') && data[0].borders.map((eachBorder) => <p key={uuidv4()} className = "inline-block p-1 text-sm">{eachBorder}</p>)
+                        }
+                    </div>                        
+                */}
+            </div>
         </div>
     )
 }
+
 
 export default Showcountry;
