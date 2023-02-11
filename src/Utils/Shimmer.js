d4a5bf1eb79dd4cc12e05f04676bc29769fdf1e7
Preetam Varun <preetamarun99@gmail.com>
2023-02-11 13:44:55 +0000

    animated shimmer UI

--- src/Utils/Shimmer.js
+++ src/Utils/Shimmer.js
@@ -6,7 +6,7 @@ const Shimmer = () => {
       <div className="flex justify-between max-w-[1024px] mx-auto mt-20">
         <div className="h-[300px] w-60 bg-slate-700 m-2 rounded-lg">
           <div className="h-1/2 translate-y-[100%] rounded-b-lg">
-            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto" id="gradient1"></p>
+            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto titleGradient"></p>
             <div className="relative h-[100%] w-[95%] m-auto">
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
@@ -16,7 +16,7 @@ const Shimmer = () => {
         </div>
         <div className="h-[300px] w-60 bg-slate-700 m-2 rounded-lg">
           <div className="h-1/2 translate-y-[100%] rounded-b-lg">
-            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto" id="gradient1"></p>
+            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto titleGradient"></p>
             <div className="relative h-[100%] w-[95%] m-auto">
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
@@ -26,7 +26,7 @@ const Shimmer = () => {
         </div>
         <div className="h-[300px] w-60 bg-slate-700 m-2 rounded-lg">
           <div className="h-1/2 translate-y-[100%] rounded-b-lg">
-            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto" id="gradient1"></p>
+            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto titleGradient"></p>
             <div className="relative h-[100%] w-[95%] m-auto">
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
@@ -36,7 +36,7 @@ const Shimmer = () => {
         </div>
         <div className="h-[300px] w-60 bg-slate-700 m-2 rounded-lg">
           <div className="h-1/2 translate-y-[100%] rounded-b-lg">
-            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto" id="gradient1"></p>
+            <p className="bg-slate-600 h-2 w-[95%] rounded-md m-auto titleGradient"></p>
             <div className="relative h-[100%] w-[95%] m-auto">
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
               <p className="bg-slate-600 h-2 w-[50%] rounded-md gradient"></p>
