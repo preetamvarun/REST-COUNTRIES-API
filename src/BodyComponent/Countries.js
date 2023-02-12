8e77f26927e61eec0f6b3de4e33adc0c658a51bf
Preetam Varun <preetamarun99@gmail.com>
2023-02-12 14:15:58 +0000

    toggling is working on input fields

--- src/BodyComponent/Countries.js
+++ src/BodyComponent/Countries.js
@@ -42,20 +42,24 @@ const Countries = ({ modeColor }) => {
   ) : (
     <div>
       <div className="flex justify-center items-start py-8 flex-col lg:flex lg:flex-row lg:items-center lg:justify-between lg:max-w-[1024px] lg:m-auto">
-        <div className="bg-slate-700 rounded-md m-3 w-5/6 md:w-1/2 lg:w-1/3">
-          <i className="fa-solid fa-magnifying-glass text-white relative pl-4 translate-y-[15%]"></i>
+        <div className={`${setBackgroundColor(modeColor)} rounded-md m-3 w-5/6 md:w-1/2 lg:w-1/3`}>
+          <i className="fa-solid fa-magnifying-glass text-gray-500 relative pl-4 translate-y-[15%]"></i>
           <input
             type="text"
             value={searchCountry}
             placeholder="search for any country..."
             onChange={handleSearchCountry}
-            className="bg-slate-700 text-gray-400 placeholder:font-semibold placeholder:text-xs placeholder:pl-1 py-3 rounded-md outline-none pl-2"
+            className={`${setBackgroundColor(
+              modeColor
+            )} text-gray-500 placeholder:font-semibold placeholder:text-xs placeholder:pl-1 py-3 rounded-md outline-none pl-2`}
           />
         </div>
         <select
           name="Region"
           id="Region"
-          className="bg-slate-600 lg:mt-3 text-gray-400 text-[12px] rounded-sm py-3 pl-2 outline-none m-3 mt-1"
+          className={`${setBackgroundColor(
+            modeColor
+          )} lg:mt-3 text-gray-500 text-[12px] rounded-sm py-3 pl-2 outline-none m-3 mt-1`}
           onChange={handleRegionFilter}
         >
           <option value="Filter By Region">Filter By Region</option>
